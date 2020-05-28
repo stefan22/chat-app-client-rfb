f45ee8758978928c819e05c7d2b4b9862ad35492
stefan22 <stefan.emailacct@gmail.com>
2020-05-28 01:19:23 +0100

    [profile] user-profile edit

--- src/theme/home.js
+++ src/theme/home.js
@@ -1,8 +1,15 @@
 export default {
   homeCards: {
-    margin: '0 1rem'
+    margin: '0 1rem',
+    position: 'relative'
   },
   homeRight: {
-    margin: '0 1rem'
-  }
+    margin: '0 1rem',
+    position: 'relative'
+  },
+  homeSpinner1: {
+    position: 'absolute',
+    top: '20vh',
+    left: '15%'
+  },
 };
