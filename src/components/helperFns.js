3b56cb635d872033335965dc6cd0631f375232e5
stefan22 <stefan.emailacct@gmail.com>
2020-06-03 13:04:33 +0100

    [refactored] created helper fn

---
+++ src/components/helperFns.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+
+export const addItemsToLocalStorage = (data) =>
+  localStorage.setItem('messages', JSON.stringify(data));
+
+export const getItemsFromLocalStorage = (name) =>
+  JSON.parse(localStorage.getItem(name));
+
+export const setAuthToken = token => {
+  let fbToken = `Bearer ${token}`;
+  localStorage.setItem('fbToken', fbToken);
+  axios.defaults.headers.common['Authorization'] = fbToken;
+}
+
+export const getAuthToken = token =>
+  localStorage.getItem(token);
