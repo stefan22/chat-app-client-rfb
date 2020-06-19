23dacccc0db073df3fd7c5ea00dee84f7cfe42bc
stefan22 <stefan.emailacct@gmail.com>
2020-06-19 20:01:09 +0100

    [userPage]init-layout

--- src/pages/user/User.js
+++ src/pages/user/User.js
@@ -2,12 +2,19 @@ import React, { Component } from 'react';
 // matui
 import Grid from '@material-ui/core/Grid';
 import withStyles from '@material-ui/core/styles/withStyles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 // redux
 import { connect } from 'react-redux';
-import { getUserProfileNMessages  } from '../../redux/actions/usersActions';
+import { getUserProfileNMessages } from '../../redux/actions/usersActions';
 // styles
 import userStyles from './user.styles';
+
 
 
 
@@ -22,42 +29,132 @@ class User extends Component {
   }
   
 
-
-
   render() {
     console.log(this);
     const { user } = this.props.match.params;
     let pageTitle = user.charAt(0).toUpperCase() + user.slice(1);
     const {
       classes,
+      userMessages
     } = this.props;
   
 
     return (
-      <Grid container>
-        <Grid item sm={12} xs={12}>
-          <div className={classes.userWrapper}>
-            <header className={classes.userHeader}>
-              <Typography 
-                className={classes.userTitle}
-                variant='h2' color='primary' align='center'>
-                {pageTitle}
-              </Typography>
-            </header>
-          </div>
+      <div className={classes.userWrapper}>
+        <Grid container spacing={4}>
+        
+            <Grid item sm={12} xs={12}>
+              <div className={classes.userWrapper}>
+                <header className={classes.userHeader}>
+                  <Typography 
+                    className={classes.userTitle}
+                    variant='h2' color='primary' align='center'>
+                    {pageTitle}
+                  </Typography>
+                </header>
+              </div>
+            </Grid>
+
+            <Grid item lg={6} md={12} sm={6} xs={12}>
+                <header className={classes.messagesHeader}>
+                  <Typography 
+                    className={classes.userSubtitle}
+                    variant='h4' align='left'
+                  >
+                    Messages
+                  </Typography>
+                </header>
+                <div className={classes.userMessages}>
+                  {!!userMessages && userMessages.length > 0 ?
+                    userMessages.map(msg => 
+                      <Card key={msg.messageId} className={classes.root} elevation={2}>
+                        <CardActionArea>
+                           <CardContent>
+                            <Typography className={classes.userName} gutterBottom variant="h4" component="h2">
+                              {msg.user}
+                            </Typography>
+                            <Typography variant="body2" className={classes.userMessage} component="p">
+                              {msg.message}
+                            </Typography>
+                          </CardContent>
+
+                          <CardMedia
+                            className={classes.userImage}
+                            component="img"
+                            alt={msg.user}
+                            height="100"
+                            image={msg.imageUrl}
+                            title={msg.user}
+                          />
+
+                        </CardActionArea>
+                       
+                      </Card>
+    
+                    ) : (
+                      'No messages'
+                    )
+                  }
+                </div>
+            </Grid>
+
+            <Grid item lg={6} md={12} sm={6} xs={12}>
+                <header className={classes.messagesHeader}>
+                  <Typography
+                    className={classes.userSubtitle}
+                    variant='h4' align='left'
+                  >
+                    Profile
+                  </Typography>
+                </header>
+                <Card className={classes.rootProfile} elevation={2}>
+                
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        alt={"user profile"}
+                        height="140"
+                        image={"#"}
+                        title={"user profile"}
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                          User name
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                          details
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                
+                  <CardActions>
+                    You must be logged in to view this.
+                    <Button size="small" color="primary">
+                      Sign up
+                    </Button>
+                    <Button size="small" color="primary">
+                      Login here
+                    </Button>
+                  </CardActions>
+                </Card>
+            </Grid>
+
+        
         </Grid>
-      </Grid>
+      </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
+  authenticated: state.user.authenticated,
   user: state.user,
+  userMessages: state.user.userMessages,
   ui: state.ui,
 });
 
 const mapActionsToProps = {
-  getUserProfileNMessages
+  getUserProfileNMessages,
 };
 
 export default connect(
--- src/pages/user/user.styles.js
+++ src/pages/user/user.styles.js
@@ -1,6 +1,9 @@
-export default {
-  userWrapper: {
+import theme from '../../theme/customTheme';
 
+export default {
+  
+  userWrapper: {
+    margin: '0 1rem'
   },
   userHeader: {
     display: 'flex',
@@ -12,4 +15,90 @@ export default {
 	 userTitle: {
     fontWeight: 400,
   },
+  // root: {
+  //   margin: 'auto',
+  //   borderRadius: '16px',
+  //   transition: '0.3s',
+  //   boxShadow: '0px 14px 80px rgba(34, 35, 58, 0.2)',
+  //   position: 'relative',
+  //   maxWidth: 500,
+  //   marginLeft: 'auto',
+  //   overflow: 'initial',
+  //   background: '#ffffff',
+  //   display: 'flex',
+  //   flexDirection: 'column',
+  //   alignItems: 'center',
+  //   paddingBottom: '2rem',
+   
+  // },
+  // media: {
+  //   width: '88%',
+  //   marginLeft: 'auto',
+  //   marginRight: 'auto',
+  //   marginTop: '3rem',
+  //   height: 0,
+  //   paddingBottom: '48%',
+  //   borderRadius: '2rem',
+  //   backgroundColor: '#fff',
+  //   position: 'relative',
+   
+  //   '&:after': {
+  //     content: '" "',
+  //     position: 'absolute',
+  //     top: 0,
+  //     left: 0,
+  //     width: '100%',
+  //     height: '100%',
+  //     backgroundImage: 'linear-gradient(147deg, #fe8a39 0%, #fd3838 74%)',
+  //     borderRadius: '16px',
+  //     opacity: 0.5,
+  //   },
+  // },
+  messagesHeader: {
+    padding: '0'
+  },
+  userName: {
+    color: theme.palette.primary.contrastText
+  },
+ 
+  content: {
+    padding: 24,
+  },
+  userMessages: {
+    marginBottom: '3rem',
+  },
+  userMessage: {
+    color: theme.palette.primary.contrastText,
+    fontWeight: 500,
+  },
+  root: {
+    margin: '2rem 0',
+    marginBottom: '3rem',
+    backgroundColor: theme.palette.secondary.light
+  },
+  rootProfile: {
+    margin: '3rem 0'
+  },
+
+  userImage: {
+    maxWidth: '60px',
+    height: 'auto',
+    padding: '5px 0',
+    marginTop: '2rem',
+    marginLeft: '1rem',
+    marginBottom: '1rem',
+    borderRadius: '50%',
+    boxShadow: `0px 1px 3px ${theme.palette.secondary.dark}`,
+  },
+  userCardMedia:{
+    padding: '1rem 0',
+    marginBottom: '3rem',
+  },
+  userSubtitle: {
+    color: theme.palette.primary.light
+  },
+  actionMessage: {
+    display: 'inline',
+    width: '50%'
+  }
 };
