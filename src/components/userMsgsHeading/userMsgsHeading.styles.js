4fd05797cd3bfa170f7018ed778c9d38c7e3e205
stefan22 <stefan.emailacct@gmail.com>
2020-06-20 07:28:37 +0100

    [usrMsgs]ongoing..

---
+++ src/components/userMsgsHeading/userMsgsHeading.styles.js
@@ -0,0 +1,17 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+
+export default makeStyles((theme) => ({
+	userHeader: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'center',
+    margin: '3rem 0 5rem',
+  },
+	 userTitle: {
+    fontWeight: 400,
+  },
+
+
+}));
